11c2aab0fe9f57688ce8fb47fd1f846b214d1cc4
gremy_c <gremy_c@etna-alternance.net>
2020-12-08 09:43:30 +0100

    added Extras_URL & variables_Model

--- MyWeather/Controller/ViewController.swift
+++ MyWeather/Controller/ViewController.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var weatherType: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var currentCityTemp: UILabel!
+    @IBOutlet weak var currentDate: UILabel!
+    @IBOutlet weak var specialBG: UIImageView!
+    
+    
+    
+    
+    // Constants
+    
+    
+    
+    // Variables
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        applyEffect()
+        
+    }
+    
+    //This function is used to apply the special effect on particular UI. The BG move when you move your phone
+    func applyEffect() {
+        specialEffect(view: specialBG, intensity: 45)
+    }
+    
+    // This function creates the special effect of parallax (the point of observation)
+    
+    // Parameters:
+    // view: UIView for UIButton, UIImage ...
+    // intensity: Intensity at which the view will move
+    func specialEffect(view: UIView, intensity: Double) {
+        let horizontalMotion = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+        horizontalMotion.minimumRelativeValue = -intensity
+        horizontalMotion.maximumRelativeValue = intensity
+        
+        let verticalMotion = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+        verticalMotion.minimumRelativeValue = -intensity
+            verticalMotion.maximumRelativeValue = intensity
+        
+        let movement  = UIMotionEffectGroup()
+        movement.motionEffects = [horizontalMotion, verticalMotion]
+        view.addMotionEffect(movement)
     }
 
 
---
+++ MyWeather/Extras/Extras.swift
@@ -0,0 +1,13 @@
+//
+//  Extras.swift
+//  MyWeather
+//
+//  Created by spirit95 on 08/12/2020.
+//
+
+import Foundation
+
+let API_URL = "http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=4c0990832e057cf279ccdd4978aea9a3"
+
+
+typealias DownloadComplete = () -> ()
